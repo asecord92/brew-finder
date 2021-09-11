9fc7569d6fa5c4b40f93345b83ec1060cdeb87f3
Thorson <thorsonb@nala.roche.com>
2021-09-11 10:43:55 -0700

    recent searches

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -25,3 +25,22 @@ searchBtn.addEventListener("click", (e) => {
     formSubmit(e);
     
 });
+//create an empty 
+var recentSearches = [];
+
+function searchFunction(city){
+
+    recentSearches.push($("#searchBrew").val());
+    $('#searchBrew').val("");
+    $('.past-brews').text("");
+
+    $.each(recentSearches, function (index, value) {
+        $('.past-brews').append("<li class='historyItem'  onclick='searchBrew("+index+")'>" + value + '</li>');
+    });
+}
+
+function addtosearchbrew(id)
+{
+    $('.past-brews').val(recentSearches[id]);
+}
+
