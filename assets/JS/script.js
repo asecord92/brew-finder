5a4dbfc8fc8d368cc82bcdf93b093fd33187a884
ajia <ajialeeholiday@gmail.com>
2021-09-15 00:11:28 -0600

    Recent searches working and local storage

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,8 +1,8 @@
 const searchBrew = document.getElementById("searchBrew");
 const searchBtn = document.getElementById("searchBtn");
 const brewColl = document.getElementById("brew-name-sub");
-const mapEl = document.getElementById("map")
-// const brewInfo = document.getElementById("brew-name-sub");
+const mapEl = document.getElementById("map");
+
 
 
 //Get Value of Search Bar
@@ -12,16 +12,14 @@ function formSubmit(e) {
     let city = searchBrew.value.split(' ').join('_');
         if (!city) {
         enterCityError();
-    } else {
-        getBrew(city);
-        
-    } 
+        } else {
+            getBrew(city);
+
+        } 
 }
 
 function getBrew(city) {
     let brewApi = "https://api.openbrewerydb.org/breweries?by_city=" + city +"&per_page=3" +"&by_type=brewpub&sort=id:desc&sort=phone:asc";
-
-    console.log(brewApi);
     fetch(brewApi).then((response)=>{
         
         if (response.ok){
@@ -50,12 +48,9 @@ function enterCityError() {
 //display brew list 
 function displayBrew (brews){
     brewColl.innerText = "";
-
         let brewLoc = document.createElement("h2");
         brewLoc.innerHTML = brews[0].city + " Breweries";
         brewColl.appendChild(brewLoc);
-
-        
 
         for(var i=0; i<brews.length; i++){
             let brew = brews[i];
@@ -71,51 +66,73 @@ function displayBrew (brews){
 
             var faveBrewAdd = document.createElement("span")
             faveBrewAdd.setAttribute("class", "icon is-right")
-            faveBrewAdd.innerHTML = "<ion-icon name='add-circle-outline' size='small' class='addFaveBtn icon is-right mx-auto my-auto'></ion-icon>"
+            faveBrewAdd.innerHTML = "<ion-icon name='add-circle-outline' size='small' class='addFaveBtn"+[i]+" icon is-right mx-auto my-auto'></ion-icon>"
             brewCard.appendChild(faveBrewAdd)
 
             brewColl.appendChild(brewCard);
-            
-            //<brewD><Brew Info></Brew Info><add fave></add fave></brewD>
-        }
-    
+        } 
 }
-
 
 // Search Button Event
 
 searchBtn.addEventListener("click", (e) => {
     formSubmit(e);
+    searchFunction();
     searchBrew.value= "";
-
     // brewColl.innerText = "";
 });
 
 document.addEventListener("keypress", (e) => {
     if (e.key === "Enter") {
         formSubmit(e);
+        searchFunction();
         searchBrew.value= "";
         
         // brewColl.innerText = "";
     }
 });
+
+
+
+// function addRecentSearchCity(recentCities) {
+//     var recentCities = searchBrew.value.trim().toUpperCase();
+//     console.log(recentCities);
+//     if (recentCities) {
+//         var pastBrews = JSON.parse(window.localStorage.getItem("pastBrews"))||[];
+//         var newBrew = recentCities
+//     };
+
+//     // save to local storage
+//     pastBrews.push(newBrew);
+//     window.localStorage.setItem("pastBrews",JSON.stringify(pastBrews));
+// }
 //create an empty 
-var recentSearches = [];
+var recentSearches
 
-function searchFunction(city){
-
+function searchFunction(){
+    recentSearches = [];
     recentSearches.push($("#searchBrew").val());
     $('#searchBrew').val("");
     $('.past-brews').text("");
 
-    $.each(recentSearches, function (index, value) {
-        $('.past-brews').append("<li class='historyItem'  onclick='searchBrew("+index+")'>" + value + '</li>');
-    });
+    if (recentSearches) {
+        var pastBrews = JSON.parse(window.localStorage.getItem("pastBrews"))||[];
+        var newBrew = recentSearches
+    };
+
+    // save to local storage
+    pastBrews.push(newBrew);
+    window.localStorage.setItem("pastBrews",JSON.stringify(pastBrews));
+    console.log(pastBrews);
+    loadRecentSearches();
 }
 
-function addtosearchbrew(id)
-{
-    $('.past-brews').val(recentSearches[id]);
+var loadRecentSearches = function() {
+    recentSearches = JSON.parse(window.localStorage.getItem("pastBrews"));
+
+    $.each(recentSearches, function (index, value) {
+        $('.past-brews').append("<li class='historyItem' onclick='displayBrew("+index+")'>" + value + '</li>');
+    });
 }
 
 
@@ -128,12 +145,13 @@ function displayLastSearch () {
 
     let lastSearch = JSON.parse(localStorage.getItem('currentCity')).split(' ').join('_');
     getBrew(lastSearch);
-    console.log(lastSearch)
 }
 
 //Map
 
 function initMap() {
+
+
     map = new google.maps.Map(mapEl, {
       center: { lat: 37.2768768, lng:  -121.93628160000002 },
       zoom: 11,
@@ -141,3 +159,4 @@ function initMap() {
   }
 
 displayLastSearch();
+loadRecentSearches();
